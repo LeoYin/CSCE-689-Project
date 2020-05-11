cce6db7f24bf0059a0bb3c1a25f3879f81e68ade
LeoYin <62576113+LeoYin@users.noreply.github.com>
2020-05-10 22:08:00 -0500

    Add files via upload

---
+++ Poject8/test_on_video.py
@@ -0,0 +1,148 @@
+from models import *
+from dataset import *
+from data.extract_frames import extract_frames
+import argparse
+import os
+import glob
+import tqdm
+from torchvision.utils import make_grid
+from PIL import Image, ImageDraw
+import skvideo.io
+import ssl
+import cv2
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+
+if __name__ == "__main__":
+    ssl._create_default_https_context = ssl._create_unverified_context
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--video_path", type=str, default="test/test_videos", help="Path to video")
+    parser.add_argument("--class_path", type=str, default="classes.txt", help="Path to class text")
+    parser.add_argument("--save_path", type=str, default="test/test_results", help="Path to save results")
+    parser.add_argument("--image_dim", type=int, default=112, help="Height / width dimension")
+    parser.add_argument("--channels", type=int, default=3, help="Number of image channels")
+    parser.add_argument("--latent_dim", type=int, default=512, help="Dimensionality of the latent representation")
+    parser.add_argument("--checkpoint_model", type=str, default="model_checkpoints/ConvLSTM.pth", help="Optional path to checkpoint model")
+    opt = parser.parse_args()
+    print(opt)
+
+    assert opt.checkpoint_model, "Specify path to checkpoint model using arg. '--checkpoint_model'"
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    input_shape = (opt.channels, opt.image_dim, opt.image_dim)
+
+    transform = transforms.Compose(
+        [
+            transforms.Resize(input_shape[-2:], Image.BICUBIC),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+
+    #labels = sorted(list(set(os.listdir(opt.dataset_path))))
+    labels = {}
+    for line in open(opt.class_path, "r"):
+            idx, cl = line.rstrip('\n').split(' ',1)
+            labels[int(idx)] = cl
+    
+    # Define model and load model checkpoint
+    model = ConvLSTM(num_classes=len(labels), latent_dim=opt.latent_dim)
+    model.to(device)
+    model.load_state_dict(torch.load(opt.checkpoint_model, map_location=torch.device('cpu')))
+    model.eval()
+
+    # Extract predictions
+    if not os.path.exists(opt.save_path):
+        os.mkdir(opt.save_path)
+    for video in glob.glob(os.path.join(opt.video_path,'*.avi')):
+        record = []
+        output_frames = []
+        video_name = video.split('.avi')[0].split('/')[-1]
+
+        cap=cv2.VideoCapture(video)
+        fps = cap.get(5)
+        lens = cap.get(7)
+        skip = lens//40
+
+        y = []
+    
+        print(video_name)
+        for j, frame in enumerate(tqdm.tqdm(extract_frames(video), desc="Processing frames")):
+            if j%skip!=0:
+                record.append([j/fps, torch.max(prediction).item()])
+                y.append(predicted_label)
+                d = ImageDraw.Draw(frame)
+                d.text(xy=(20, 20), text=predicted_label, fill=(255, 255, 255))
+
+                output_frames += [frame]
+                continue
+
+            image_tensor1 = Variable(transform(frame)).to(device)
+            image_tensor1 = image_tensor1.view(1, 1, *image_tensor1.shape)
+            if j==0:
+                first_frame = frame
+                resize_dim = 600
+                first_frame = np.array(first_frame)
+                max_dim = max(first_frame.shape)
+                scale = resize_dim/max_dim
+                first_frame = cv2.resize(first_frame, None, fx=scale, fy=scale)
+                prev_gray = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
+                mask = np.zeros_like(first_frame)
+                mask[..., 1] = 255
+
+            gray = cv2.cvtColor(np.float32(frame), cv2.COLOR_BGR2GRAY)
+            gray = cv2.resize(gray, None, fx=scale, fy=scale)
+            flow = cv2.calcOpticalFlowFarneback(prev_gray, gray, None, pyr_scale = 0.5, levels = 5, winsize = 11, iterations = 5, poly_n = 5, poly_sigma = 1.1, flags = 0)
+            magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
+            mask[..., 0] = angle * 180 / np.pi / 2
+            mask[..., 2] = cv2.normalize(magnitude, None, 0, 255, cv2.NORM_MINMAX)
+            rgb = cv2.cvtColor(mask, cv2.COLOR_HSV2BGR)
+            frame2 = cv2.resize(np.float32(frame), None, fx=scale, fy=scale)
+            dense_flow = cv2.addWeighted(frame2, 1,rgb, 2, 0, dtype=cv2.CV_32F)
+            dense_flow = Image.fromarray(dense_flow.astype('uint8')).convert('RGB')
+            
+            image_tensor = Variable(transform(frame)).to(device)
+            image_tensor = image_tensor.view(1, 1, *image_tensor.shape)
+
+            # Get label prediction for frame
+            with torch.no_grad():
+                prediction = model(image_tensor1,image_tensor)
+                label_id = prediction.argmax(1).item()
+                predicted_label = labels[label_id]
+                record.append([j/fps, torch.max(prediction).item()])
+                y.append(predicted_label)
+
+            # Draw label on frame
+            d = ImageDraw.Draw(frame)
+            d.text(xy=(20, 20), text=predicted_label, fill=(255, 255, 255))
+
+            output_frames += [frame]
+
+        # Create video from frames
+        #height, width = output_frames[0].size
+        #size = (width,height)
+        #out = cv2.VideoWriter(opt.save_path+video_name+'.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+        #for i in range(len(output_frames)):
+        #    out.write(output_frames[i])
+        #out.release()
+        writer = skvideo.io.FFmpegWriter(os.path.join(opt.save_path,video_name+'.avi'))
+        for frame in tqdm.tqdm(output_frames, desc="Writing to video"):
+            writer.writeFrame(np.array(frame))
+        writer.close()
+
+
+        json_str = json.dumps(record)
+        with open(opt.save_path+'/timeLabel_'+video_name+'.json', 'w') as json_file:
+            json_file.write(json_str)
+
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+        ax1.set_title('Plot '+video_name)
+        plt.xlabel('Time')
+        plt.ylabel('Label')
+        x = np.arange(0,len(y))/fps
+        ax1.scatter(x,y,c = 'r',marker = 'o')
+        plt.savefig(opt.save_path+'/'+video_name+'_plot.png',bbox_inches='tight')
+    
+    
